4d059d1102d75ee71557f721901e4ddf01d2e590
Michael <michael.mircic@hotmail.coml>
2020-09-01 15:34:26 -0400

    Completed textbox constructor

--- src/Helpers/TextBox.cpp
+++ src/Helpers/TextBox.cpp
@@ -6,72 +6,66 @@
  * This class will be used to easily place and reposition groupings
  * of text within the game window.
  * Functions required:
- * 	-hold text within an sf::Rectangle
+ * 	-hold text within a rectangle
  * 	-automatically enter a newline when text can no longer fit in the rectangle
  * 	-allow text to be written fully or one character at a time
  * 	-allow for size changes (text will need to be rewritten into the box)
  * 	-(optional) make text fill line (increase character spacing) (gonna need my own font for this)
+ * 	-allow for centered or square text
  */
 
 /*
- * The class will take a boolean value to say whether the text will be cascaded, as well as the
+ * The class will take a short value for cascade speed, as well as the
  * already positioned rectangle from the calling function. The method reformatText will be
  * used to fit the text into the box initially, as well as any other time this would need
  * to be performed.
  */
-void reformatText(sf::RectangleShape, sf::Text);
-void changeSpeed(short);
 
-short speed;
 
 //confusing naming due to late changes
 struct TextBlock
 {
 	sf::RectangleShape box;
 	sf::Text text;
-	bool cascade;
+	short speed;
 	sf::Font font;
 };
 
-//no bool will instantiate to false
-TextBlock TextBox(sf::Vector2f boxOutline, sf::Text inputText, sf::Font inputFont) {
+void reformatText(TextBlock);
+void setPosition(TextBlock);
+void changeSpeed(TextBlock, short);
+short getSpeed(TextBlock);
+
+TextBlock TextBox(sf::Vector2f boxOutline, sf::Text inputText, short inputSpeed, sf::Font inputFont) {
 
 	TextBlock textBox;
 	textBox.box.setSize(boxOutline);
 	textBox.text.setString(inputText.getString());
-	textBox.cascade = false;
+	textBox.speed = inputSpeed;
 	textBox.font = inputFont;
-
 
 	return textBox;
 }
 
-sf::Text TextBox(sf::RectangleShape boxOutline, sf::Text inputText, bool cascade) {
+//reformat the text (done on initialization as well as whenever required)
+void reformatText(TextBlock textBox) {
 
-	sf::Text text;
-	sf::Font font;
-	if (!font.loadFromFile("Assets/Fonts/Raleway-Regular.ttf")) {
-		std::cout << "Couldn't load the font file." << std::endl;
-		exit(1);
-	}
-
-	speed = 2;
-
-	return text;
 }
 
-//reformat the text (done on initialization as well as whenever required)
-void reformatText(sf::RectangleShape boxOutline, sf::Text text) {
+//set the position of the box (will need to reposition and possibly reformat text as well)
+void setPosition (TextBlock textBox){
 
+
+	reformatText(textBox);
 }
 
 //change text cascade speed (probably 3 different speeds)
-void changeSpeed(short speedChange){
-	speed = speedChange;
+void changeSpeed(TextBlock textBox, short speedChange){
+	textBox.speed = speedChange;
 }
 
-short getSpeed(){
-	return speed;
+short getSpeed(TextBlock textBox){
+	return textBox.speed;
 }
 
 
